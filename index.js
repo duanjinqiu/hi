cb14e7240ff01192e8b603378f09f76d4b71d87f
duanjinqiu <471568720@qq.com>
2023-01-17 22:20:13 +0800

    增加文件夹路径判断 增加.gitignore文件

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const KoaRouter = require("koa-router");
 const router = new KoaRouter();
 const koaStatic = require("koa-static");
 const path = require("path");
+const fs = require("fs");
+const promisify = require("util").promisify;
+const promiseStat = promisify(fs.stat);
+
+const staticFolder = path.resolve("./");
+console.log(staticFolder);
+try {
+  promiseStat(staticFolder).then((a) => {
+    console.log(a);
+  });
+} catch (err) {
+  console.error(err);
+}
 
 app.use(koaStatic(path.resolve("./static")));
 router.get("/giao", async (ctx) => {
