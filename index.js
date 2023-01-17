c1c164615fabe7cb7334a5f9e6b3d8ebc729f262
duanjinqiu <471568720@qq.com>
2023-01-17 22:26:28 +0800

    修改代码，是接口能获取到文件夹信息

--- index.js
+++ index.js
@@ -9,18 +9,19 @@ const promisify = require("util").promisify;
 const promiseStat = promisify(fs.stat);
 
 const staticFolder = path.resolve("./");
-console.log(staticFolder);
-try {
-  promiseStat(staticFolder).then((a) => {
-    console.log(a);
-  });
-} catch (err) {
-  console.error(err);
-}
 
 app.use(koaStatic(path.resolve("./static")));
 router.get("/giao", async (ctx) => {
   ctx.body = "giao";
+});
+router.get("/getFolderState", async (ctx) => {
+  try {
+    const huluwa = await promiseStat(staticFolder);
+    ctx.body = { huluwa, staticFolder };
+  } catch (err) {
+    ctx.body = "出错了!!!";
+    console.error(err);
+  }
 });
 
 app.use(router.routes());
